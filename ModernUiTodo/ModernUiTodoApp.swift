a58873e677d718d592dc79c4130ce68719c11de6
Harsh Soni <harsh.here17@gmail.com>
2023-05-04 11:54:06 +0530

    Initial Commit

---
+++ ModernUiTodo/ModernUiTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModernUiTodoApp.swift
+//  ModernUiTodo
+//
+//  Created by Harsh Soni on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ModernUiTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
